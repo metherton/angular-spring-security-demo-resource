42cc4bd44f81b4c4314bdccd40637237aa1a3550
metherton <martinetherton63@googlemail.com>
2015-10-06 22:26:12 +0200

    added files

---
+++ src/main/java/demo/ResourceApplication.java
@@ -0,0 +1,38 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+@EnableRedisHttpSession
+class ResourceApplication {
+
+    @RequestMapping("/")
+    public Map<String,Object> home() {
+        Map<String,Object> model = new HashMap<String,Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceApplication.class, args);
+    }
+
+    @Bean
+    HeaderHttpSessionStrategy sessionStrategy() {
+        return new HeaderHttpSessionStrategy();
+    }
+
+}
