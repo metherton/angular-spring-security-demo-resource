ff42da978f76a78b8f25a668a422efd682a29bc8
metherton <martinetherton63@gmail.com>
2017-06-14 21:49:30 +0200

    reenable tests

--- src/test/java/demo/ResourceApplicationTests.groovy
+++ src/test/java/demo/ResourceApplicationTests.groovy
@@ -10,7 +10,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 @RunWith(SpringJUnit4ClassRunner)
 @SpringApplicationConfiguration(classes = ResourceApplication)
 @WebAppConfiguration
-@Ignore
 class ResourceApplicationTests {
 
 	@Test
